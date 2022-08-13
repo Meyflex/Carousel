872197a97a6ea42434c5c93008ebc375c35b7721
meyflex <mohamdimagh1@gmail.com>
2022-08-13 17:06:39 +0300

    automate transitions

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const slides=document.getElementsByClassName("carousel-item");
+const dots=document.getElementsByClassName("Dot");
 let slideCompteur=0;
 const slidesTail=slides.length;
-
+const boucle=1;
 
 document.getElementById("carousel-button-next").addEventListener('click',clickdroit)
 document.getElementById("carousel-button-prev").addEventListener('click',clickgauche)
+
 function clickdroit(){
     hideSlides();
     if(slideCompteur===slidesTail-1){
@@ -13,7 +15,9 @@ function clickdroit(){
         slideCompteur++;
     }
     slides[slideCompteur].classList.add("carousel-item-visible")
+    dots[slideCompteur].classList.add("image-dot")
 }
+
 function clickgauche(){
     hideSlides();
     if(slideCompteur===0){
@@ -21,10 +25,20 @@ function clickgauche(){
     }else{
         slideCompteur--;
     }
-    slides[slideCompteur].classList.add("carousel-item-visible")}
+    slides[slideCompteur].classList.add("carousel-item-visible")
+    dots[slideCompteur].classList.add("image-dot")
+
+}
 function hideSlides(){
-    for( let name of slides){
-        name.classList.remove("carousel-item-visible")
-        name.classList.add("hidden")
+    for( let i=0;i<slidesTail;i++){
+        slides[i].classList.remove("carousel-item-visible")
+        slides[i].classList.add("hidden")
+
+        dots[i].classList.remove("image-dot")
     }
 }
+
+function timeout() {
+    setTimeout(function () { clickdroit();timeout();}, 5000);
+}
+timeout(); 
